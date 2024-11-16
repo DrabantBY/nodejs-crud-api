23da0b7dd306e4e5747124c97c501a8ef6e8fa61
DrabantBY <drabant2022@gmail.com>
2024-11-16 21:10:08 +0300

    refactor: add users map collection

--- src/db.js
+++ src/db.js
@@ -1,35 +1,50 @@
-export const users = [
-	{
-		id: '00000000-0000-0000-0000-000000000000',
-		username: 'Eugene',
-		age: 37,
-		hobbies: ['bjj', 'books', 'node.js'],
-	},
-	{
-		id: 'ffffffff-ffff-ffff-ffff-ffffffffffff',
-		username: 'John',
-		age: 24,
-		hobbies: ['javascript', 'typescript'],
-	},
+export const users = new Map([
+	[
+		'00000000-0000-0000-0000-000000000000',
+		{
+			id: '00000000-0000-0000-0000-000000000000',
+			username: 'Eugene',
+			age: 37,
+			hobbies: ['bjj', 'books', 'node.js'],
+		},
+	],
+	[
+		'ffffffff-ffff-ffff-ffff-ffffffffffff',
+		{
+			id: 'ffffffff-ffff-ffff-ffff-ffffffffffff',
+			username: 'John',
+			age: 24,
+			hobbies: ['javascript', 'typescript'],
+		},
+	],
 
-	{
-		id: '123e4567-e89b-12d3-a456-426614174000',
-		username: 'Alex',
-		age: 31,
-		hobbies: [],
-	},
+	[
+		'123e4567-e89b-12d3-a456-426614174000',
+		{
+			id: '123e4567-e89b-12d3-a456-426614174000',
+			username: 'Alex',
+			age: 31,
+			hobbies: [],
+		},
+	],
 
-	{
-		id: '92f62d9e-22c4-11ef-97e9-325096b39f47',
-		username: 'Max',
-		age: 40,
-		hobbies: ['drive', 'weapon'],
-	},
+	[
+		'92f62d9e-22c4-11ef-97e9-325096b39f47',
+		{
+			id: '92f62d9e-22c4-11ef-97e9-325096b39f47',
+			username: 'Max',
+			age: 40,
+			hobbies: ['drive', 'weapon'],
+		},
+	],
 
-	{
-		id: '01695553-c90c-705a-b56d-778dfbbd4bed',
-		username: 'Adam',
-		age: 22,
-		hobbies: ['football'],
-	},
-];
+	[
+		'01695553-c90c-705a-b56d-778dfbbd4bed',
+		{
+			id: '01695553-c90c-705a-b56d-778dfbbd4bed',
+			username: 'Adam',
+			age: 22,
+			hobbies: ['football'],
+		},
+	],
+]);
