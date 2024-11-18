f0897fcfae5710587c907a90033174282001d06d
DrabantBY <drabant2022@gmail.com>
2024-11-19 00:17:27 +0300

    feat: add base route server tests

--- src/db.ts
+++ src/db.ts
@@ -1,33 +1,3 @@
 import type { User } from './types.ts';
 
-export const users = new Map<string, User>([
-	[
-		'123e4567-e89b-12d3-a456-426614174000',
-		{
-			id: '123e4567-e89b-12d3-a456-426614174000',
-			username: 'Eugene',
-			age: 27,
-			hobbies: ['next.js', 'angular', 'node.js'],
-		},
-	],
-
-	[
-		'92f62d9e-22c4-11ef-97e9-325096b39f47',
-		{
-			id: '92f62d9e-22c4-11ef-97e9-325096b39f47',
-			username: 'MadMax',
-			age: 40,
-			hobbies: ['cars', 'fire arms'],
-		},
-	],
-
-	[
-		'01695553-c90c-705a-b56d-778dfbbd4bed',
-		{
-			id: '01695553-c90c-705a-b56d-778dfbbd4bed',
-			username: 'Terminator',
-			age: 32,
-			hobbies: [],
-		},
-	],
-]);
+export const users = new Map<string, User>();
--- test/server.test.ts
+++ test/server.test.ts
@@ -1,10 +1,44 @@
 import request from 'supertest';
+import { validate } from 'uuid';
 import { server } from '../src/server';
 
-describe('Server Tests', () => {
-	it('should return status 200', async () => {
-		const response = await request(server).get('/api/users');
-		expect(response.status).toBe(200);
-		// expect(response.body.status).toBe('ok');
+const user = {
+	username: 'Eugene',
+	age: 37,
+	hobbies: ['next.js', 'angular', 'node.js'],
+};
+
+describe('test server by route /api/users', () => {
+	it('should return empty user list', async () => {
+		const { status, body } = await request(server).get('/api/users');
+
+		expect(status).toBe(200);
+
+		expect(body).toEqual([]);
+	});
+
+	it('should create new user in current user list', async () => {
+		const { status, body } = await request(server)
+			.post('/api/users')
+			.send(user);
+
+		expect(status).toBe(201);
+
+		expect(body).toMatchObject({
+			code: 201,
+			message: 'user successfully created',
+		});
+	});
+
+	it('should return user list with created user', async () => {
+		const { status, body } = await request(server).get('/api/users');
+
+		expect(status).toBe(200);
+
+		expect(body).toHaveLength(1);
+
+		expect(validate(body[0].id)).toBe(true);
+
+		expect(body[0]).toMatchObject(user);
 	});
 });
