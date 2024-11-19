99fcab97121be81eb20a6a536fee4a7f988b345c
DrabantBY <drabant2022@gmail.com>
2024-11-19 14:15:43 +0300

    feat: add tests for base route

--- test/server.test.ts
+++ test/server.test.ts
@@ -9,36 +9,52 @@ const user = {
 };
 
 describe('test server by route /api/users', () => {
-	it('should return empty user list', async () => {
+	it('should get empty user list', async () => {
 		const { status, body } = await request(server).get('/api/users');
-
 		expect(status).toBe(200);
-
 		expect(body).toEqual([]);
 	});
 
-	it('should create new user in current user list', async () => {
+	it('should post new user', async () => {
 		const { status, body } = await request(server)
 			.post('/api/users')
 			.send(user);
 
 		expect(status).toBe(201);
-
 		expect(body).toMatchObject({
 			code: 201,
 			message: 'user successfully created',
 		});
 	});
 
-	it('should return user list with created user', async () => {
+	it('should get new user list', async () => {
 		const { status, body } = await request(server).get('/api/users');
 
 		expect(status).toBe(200);
-
 		expect(body).toHaveLength(1);
-
 		expect(validate(body[0].id)).toBe(true);
-
 		expect(body[0]).toMatchObject(user);
+	});
+
+	it('should be error on unimplemented method', async () => {
+		const { status, body } = await request(server).delete('/api/users');
+
+		expect(status).toBe(501);
+		expect(body).toMatchObject({
+			code: 501,
+			message: 'method not implemented',
+		});
+	});
+
+	it('should be error by post invalid data', async () => {
+		const { status, body } = await request(server)
+			.post('/api/users')
+			.send({ age: 20 });
+
+		expect(status).toBe(400);
+		expect(body).toMatchObject({
+			code: 400,
+			message: 'user has invalid data',
+		});
 	});
 });
